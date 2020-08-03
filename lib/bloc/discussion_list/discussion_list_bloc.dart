544a9c9feb4a3857b06da6a2f187979e5792fe23
jasondellaluce <jason.dellaluce@studio.unibo.it>
2020-08-03 14:05:35 +0200

    Add bloc for chat list actions

--- lib/bloc/discussion_list/discussion_list_bloc.dart
+++ lib/bloc/discussion_list/discussion_list_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:bloc/bloc.dart';
 import 'package:delphis_app/bloc/me/me_bloc.dart';
@@ -23,16 +24,107 @@ class DiscussionListBloc
   Stream<DiscussionListState> mapEventToState(
     DiscussionListEvent event,
   ) async* {
+    // TODO: Format user-friendly errors
     var prevList = this.state.discussionList;
-    if(event is DiscussionListFetchEvent && !(this.state is DiscussionListLoading)) {
+    if (event is DiscussionListFetchEvent &&
+        !(this.state is DiscussionListLoading)) {
+      yield DiscussionListLoading(
+        discussionList: prevList,
+        timestamp: DateTime.now(),
+      );
       try {
-        yield DiscussionListLoading(discussionList: prevList, timestamp: DateTime.now());
         var newList = await this.repository.getDiscussionList();
-        yield DiscussionListLoaded(discussionList: newList, timestamp: DateTime.now());
+        yield DiscussionListLoaded(
+          discussionList: newList,
+          timestamp: DateTime.now(),
+        );
+      } catch (error) {
+        yield DiscussionListError(
+          discussionList: prevList,
+          error: error,
+          timestamp: DateTime.now(),
+        );
       }
-      catch (error) {
-        // TODO: Format user-friendly error
-        yield DiscussionListError(discussionList: prevList, error: error,timestamp: DateTime.now());
+    } else if (event is DiscussionListDeleteEvent) {
+      var listWithoutElement =
+          prevList.where((e) => e.id != event.discussion.id).toList();
+      /* Yield list excluding the element to be removed. This can provide
+         a better UX, as the user thinks that the removal happened istantaneously.
+         we add it back with an error message in case something goes wrong. */
+      yield DiscussionListLoading(
+        discussionList: listWithoutElement,
+        timestamp: DateTime.now(),
+      );
+      try {
+        /* TODO: This is mocked behavior for UI testing. We need to hook this
+           up with repositories and real backend mutations. */
+        Future.delayed(Duration(seconds: 2));
+        if (Random().nextInt(3) == 0) {
+          throw "some random error";
+        }
+
+        /* Yield list excluding the removed element */
+        yield DiscussionListLoaded(
+          discussionList: listWithoutElement,
+          timestamp: DateTime.now(),
+        );
+      } catch (error) {
+        /* Yield list including the element we failed to remove */
+        yield DiscussionListError(
+          discussionList: prevList,
+          error: error,
+          timestamp: DateTime.now(),
+        );
+      }
+    } else if (event is DiscussionListMuteEvent) {
+      yield DiscussionListLoading(
+        discussionList: prevList,
+        timestamp: DateTime.now(),
+      );
+      try {
+        /* TODO: This is mocked behavior for UI testing. We need to hook this
+           up with repositories and real backend mutations. */
+        Future.delayed(Duration(seconds: 2));
+        if (Random().nextInt(3) == 0) {
+          throw "some random error";
+        }
+
+        /* Yield list with updated element */
+        yield DiscussionListLoaded(
+          discussionList: prevList,
+          timestamp: DateTime.now(),
+        );
+      } catch (error) {
+        yield DiscussionListError(
+          discussionList: prevList,
+          error: error,
+          timestamp: DateTime.now(),
+        );
+      }
+    } else if (event is DiscussionListArchiveEvent) {
+      yield DiscussionListLoading(
+        discussionList: prevList,
+        timestamp: DateTime.now(),
+      );
+      try {
+        /* TODO: This is mocked behavior for UI testing. We need to hook this
+           up with repositories and real backend mutations. */
+        Future.delayed(Duration(seconds: 2));
+        if (Random().nextInt(3) == 0) {
+          throw "some random error";
+        }
+
+        /* Yield list with updated element */
+        yield DiscussionListLoaded(
+          discussionList: prevList,
+          timestamp: DateTime.now(),
+        );
+      } catch (error) {
+        yield DiscussionListError(
+          discussionList: prevList,
+          error: error,
+          timestamp: DateTime.now(),
+        );
       }
     }
   }
--- lib/bloc/discussion_list/discussion_list_event.dart
+++ lib/bloc/discussion_list/discussion_list_event.dart
@@ -14,3 +14,41 @@ class DiscussionListFetchEvent extends DiscussionListEvent {
   @override
   List<Object> get props => [this.now];
 }
+
+class DiscussionListDeleteEvent extends DiscussionListEvent {
+  final DateTime now;
+  final Discussion discussion;
+
+  DiscussionListDeleteEvent(this.discussion)
+      : this.now = DateTime.now(),
+        super();
+
+  @override
+  List<Object> get props => [this.now, this.discussion];
+}
+
+class DiscussionListArchiveEvent extends DiscussionListEvent {
+  final DateTime now;
+  final Discussion discussion;
+  final bool archived;
+
+  DiscussionListArchiveEvent(this.discussion, this.archived)
+      : this.now = DateTime.now(),
+        super();
+
+  @override
+  List<Object> get props => [this.now, this.discussion, this.archived];
+}
+
+class DiscussionListMuteEvent extends DiscussionListEvent {
+  final DateTime now;
+  final Discussion discussion;
+  final bool muted;
+
+  DiscussionListMuteEvent(this.discussion, this.muted)
+      : this.now = DateTime.now(),
+        super();
+
+  @override
+  List<Object> get props => [this.now, this.discussion, this.muted];
+}

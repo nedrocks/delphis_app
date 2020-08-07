459cb4fd6a0be19fe1c00654c1e748632ae33ea5
Ned Rockson <nedrocks@gmail.com>
2020-08-07 14:53:18 -0700

    Remove debug print.

--- lib/bloc/observer.dart
+++ lib/bloc/observer.dart
@@ -3,7 +3,6 @@ import 'package:bloc/bloc.dart';
 class ChathamBlocObserver extends BlocObserver {
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    print('bloc: ${bloc} transition: ${transition.event}');
     super.onTransition(bloc, transition);
   }
 }
